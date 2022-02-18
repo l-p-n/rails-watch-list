4ce93d6a7c1b70ce941db96d104aa88a70699ce2
Luiza Passaglia Nascimento <luizapn@hotmail.com>
2022-02-18 12:32:39 +0000

    correction to bookmark destroy function

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,15 +1,18 @@
 class BookmarksController < ApplicationController
+  before_action :set_bookmark, only: :destroy
+  before_action :set_list, only: [:new, :create]
+
   def new
     # need @list in our `simple_form_for`
-    @list = List.find(params[:list_id])
+    # set_list
     @bookmark = Bookmark.new
   end
 
   def create
     @bookmark = Bookmark.new(bookmark_params)
     # need `list_id` to associate bookmark with corresponding list
-    @list = List.find(params[:list_id])
-    @bookmark.list_id = @list.id
+    # set_list
+    @bookmark.list = @list # same as @bookmark.list_id = @list.id
     if @bookmark.save
       redirect_to list_path(@list)
     else
@@ -18,12 +21,20 @@ class BookmarksController < ApplicationController
   end
 
   def destroy
-    @bookmark = Bookmark.find(params[:id])
+    # set_bookmark
     @bookmark.destroy
-    redirect_to list_path(@list)
+    redirect_to list_path(@bookmark.list)
   end
 
   private
+
+  def set_bookmark
+    @bookmark = Bookmark.find(params[:id])
+  end
+
+  def set_list
+    @list = List.find(params[:list_id])
+  end
 
   def bookmark_params
     params.require(:bookmark).permit(:comment, :movie_id)
