265451716a30d887e062aa96de48487e6e4d7d78
Luiza Passaglia Nascimento <luizapn@hotmail.com>
2022-04-19 18:41:53 +0100

    added backdrop_url column to Movie model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_18_152518) do
+ActiveRecord::Schema.define(version: 2022_04_19_173851) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -66,6 +66,7 @@ ActiveRecord::Schema.define(version: 2022_02_18_152518) do
     t.integer "rating"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "backdrop_url"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
