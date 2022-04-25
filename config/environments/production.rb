e2bb5644fe7466b65545297c8b5b37094f419a7e
Luiza Passaglia Nascimento <luizapn@hotmail.com>
2022-04-25 17:26:17 +0100

    updates for deployment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -118,5 +118,5 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
-  config.tmdb_api_key = ENV.fetch('TMDB_API_KEY')
+  # config.tmdb_api_key = ENV.fetch('TMDB_API_KEY')
 end
